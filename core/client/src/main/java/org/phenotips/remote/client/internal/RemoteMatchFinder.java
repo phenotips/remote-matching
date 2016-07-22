06c61555a3739cbbf44421e55afcd8592cb71fde
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-07-21 20:24:43 -0400

    Fixed creating an incoming match.

--- core/client/src/main/java/org/phenotips/remote/client/internal/RemoteMatchFinder.java
+++ core/client/src/main/java/org/phenotips/remote/client/internal/RemoteMatchFinder.java
@@ -119,7 +119,7 @@ public class RemoteMatchFinder implements MatchFinder
 
         List<RemotePatientSimilarityView> parsedResults = this.matchingService.getSimilarityResults(request);
         for (RemotePatientSimilarityView result : parsedResults) {
-            PatientMatch match = new DefaultPatientMatch(result, remoteId, true);
+            PatientMatch match = new DefaultPatientMatch(result, null, remoteId);
             patientMatchList.add(match);
         }
         return patientMatchList;
