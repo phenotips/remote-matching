f84b3e6b2f067603607b82d626fe75ded10cf26c
veronikaslc <veronika.koltunov@gmail.com>
2017-07-11 16:54:51 -0400

    RM-59: Allow for back-end pagination of remote matches Sort remote matches by internal similarity score in the RemoteMatchedPatientClusterView constructor

--- core/common/src/main/java/org/phenotips/remote/common/internal/RemoteMatchedPatientClusterView.java
+++ core/common/src/main/java/org/phenotips/remote/common/internal/RemoteMatchedPatientClusterView.java
@@ -23,6 +23,7 @@ import org.phenotips.data.similarity.PatientSimilarityView;
 import org.phenotips.remote.api.OutgoingMatchRequest;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -80,6 +81,18 @@ public class RemoteMatchedPatientClusterView implements MatchedPatientClusterVie
         this.reference = patient;
         this.response = response;
         this.remoteMatches = remoteMatches;
+
+        // Sort by score
+        Collections.sort(this.remoteMatches, new Comparator<RemotePatientSimilarityView>()
+        {
+            @Override
+            public int compare(RemotePatientSimilarityView o1, RemotePatientSimilarityView o2)
+            {
+                double score1 = o1.getScore();
+                double score2 = o2.getScore();
+                return (int) Math.signum(score2 - score1);
+            }
+        });
     }
 
     @Override
